3215450d304a8460f87c1c3797103bb99954bb94
acidhood999 <nazarok616@mail.ru>
2023-12-23 22:03:25 +0100

    Добавьте файлы проекта.

---
+++ STRING_PROBEL.cpp
@@ -0,0 +1,43 @@
+﻿// STRING_PROBEL.cpp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string squeezeText(const string& pishem) 
+{
+    string shzat;
+    bool proverkaST = false;
+
+    for (char probel : pishem)
+    {
+        if (probel == ' ')
+        {
+            if (!proverkaST)
+            {
+                shzat += ' ';
+                proverkaST = true;
+            }
+        }
+        else 
+        {
+            shzat += probel;
+            proverkaST = false;
+        }
+    }
+    return shzat;
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "RU");
+
+    string proverka1 = "      МОТИВАЦИЮ     НАДО     ПОДНЯТЬ       !!!!!!!";
+    string proverkaSHZATIA = squeezeText(proverka1);
+
+    cout << "Текст без сжатия: " << proverka1 << endl;
+    cout << "Текст с сжатием: " << proverkaSHZATIA << endl;
+
+   
+}
